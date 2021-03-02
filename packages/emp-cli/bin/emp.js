352e92d55c29bb72373f542b0d4852c8614e4f82
zhaoxiaoguang <zhaoxiaoguang@zuoyebang.com>
2021-03-02 19:51:32 +0800

    添加类型声明文件本地同步命令 deoloy

--- packages/emp-cli/bin/emp.js
+++ packages/emp-cli/bin/emp.js
@@ -180,5 +180,15 @@ program
       })
   })
 
+// 增加声明文件在本地的同步功能
+program
+  .command('deploy')
+  .option('-t, --ts', '生成类型文件 默认为 false')
+  .option('-n, --createName <createName>', '文件名 默认为 index.d.ts [* 使用默认值更方便同步]')
+  .option('-p, --createPath <createPath>', '相对命令行目录 默认为 dist')
+  .action(({ts, createName, createPath}) => {
+    require('../scripts/deploy')({ts, createName, createPath})
+  })
+
 // 执行命令
 program.parse(process.argv)
